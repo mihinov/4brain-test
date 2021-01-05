a203412067d2250405b2c8b6f1e65634d11cd50c
mihinov <kamahinmihail@gmail.com>
2021-01-05 19:18:14 +0300

    go

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 class Quiz {
   constructor(options) {
-    console.log(options.nameForm);
     this.formDOM = document.querySelector(`#${options.nameForm}`);
     const radioS = Array.from(this.formDOM?.querySelectorAll(`[name=${options.nameInputRadio}]`));
     this.radioS = this.cloneRadios(radioS);
     this.deleteRadios(radioS);
+    this.fieldsRadio = this.createFieldsRadio(this.formDOM, this.radioS);
   }
 
   cloneRadios(radiosDom) {
@@ -21,8 +21,75 @@ class Quiz {
     }
   }
 
-  createFields(radiosDom) {
-    
+  createFieldsRadio(formDOM, radiosDom) {
+    const arrFields = [];
+    let clickCounter = 0;
+
+    const mouseOverFieldRadio = (e) => {
+      const fieldRadio = e.target.offsetParent;
+      fieldRadio.classList.add('hover');
+    }
+
+    const mouseOutFieldRadio = (e) => {
+      const fieldRadio = e.target.offsetParent;
+      fieldRadio.classList.remove('hover');
+    }
+
+    for (let i = 0; i < radiosDom.length; i++) {
+      const fieldRadio = document.createElement('div');
+      const value = radiosDom[i].value;
+
+      fieldRadio.classList.add('fieldRadio');
+      fieldRadio.innerHTML = `
+        <div class="fieldRadio__text">${value}</div>
+        <div class="fieldRadio__voting"></div>
+        <div class="fieldRadio__statistics"></div>
+      `;
+      fieldRadio.setAttribute('data-obj', value);
+      fieldRadio.addEventListener('click', (e) => {
+        clickCounter++;
+        if (clickCounter === 1) {
+          fieldRadio.classList.remove('hover');
+          // fieldRadio.classList.add('active');
+          arrFields.forEach(item => {
+            item.removeEventListener('mouseover', mouseOverFieldRadio);
+            item.removeEventListener('mouseout', mouseOutFieldRadio);
+          });
+          this.getStatistics().then((statistics) => {
+            statistics[value] = statistics[value] + 1;
+            this.statistics = Object.assign({}, statistics);
+            const sum = Object.keys(statistics).reduce((sum, current) => statistics[current] + sum, 0);
+            arrFields.forEach((item, i) => {
+              item.classList.add('active');
+              const itemValue = item.getAttribute('data-obj');
+              const percent = statistics[itemValue] * 10;
+              item.querySelector('.fieldRadio__statistics').innerHTML = `${percent} %`;
+            });
+            fieldRadio.classList.add('click');
+          });
+        }
+      });
+
+      fieldRadio.addEventListener('mouseover', mouseOverFieldRadio);
+      fieldRadio.addEventListener('mouseout', mouseOutFieldRadio);
+      arrFields.push(fieldRadio);
+      formDOM.append(fieldRadio);
+    }
+    return arrFields.slice();
+  }
+
+  getStatistics() { // тут получение данных из бд
+    return new Promise((res, rej) => {
+      try {
+        res({
+            "Интроверт": 3,
+            "Экстраверт": 4,
+            "Не знаю": 2
+        });
+      } catch (e) {
+        rej(e);
+      }
+    });
   }
 }
 
